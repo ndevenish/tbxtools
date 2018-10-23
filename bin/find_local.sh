4633e081555b845091eb8941ddd5c8a2add5064c
Nicholas Devenish <ndevenish@gmail.com>
2018-10-23 11:44:41 +0100

    Expand the find-local script some

--- bin/find_local.sh
+++ bin/find_local.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 #
-# Find all local repositories, then show changes that are not upstream
+# Finds and lists commits that are not present upstream.
+#
+# Searches in and under the current directory for all .git repositories.
+# These are then checked for any local commits that are not present in
+# any of the upstream repositories, e.g. commits that have been made
+# locally and do not have a known backup anywhere else.
+#
+# This is helpful to know if e.g. a repository can be safely deleted
+# without manually checking every branch for upstream matches.
+#
 
+print_usage() {
+  echo "Usage: find_local.sh [-h | --help] [search_dir]"
+}
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
-  echo "Find all local repositories, then list changes that are not upstream"
+  print_usage
+  echo
+  echo "Find all local repositories, then list changes that are not upstream."
   exit 0
 fi
 
@@ -11,16 +25,28 @@ RED=$(tput setaf 1)
 BOLD=$(tput bold)
 NC=$(tput sgr0)
 
+if [[ -n "$1" ]]; then
+  SEARCHDIR=${1%/}
+  if [[ ! -d "$SEARCHDIR" ]]; then
+    print_usage
+    echo "Error: $SEARCHDIR is not a directory"
+    exit 1
+  fi
+else
+  SEARCHDIR="."
+fi
+
 printf "Finding repos...."
-repo_dirs=$(find -L . -name ".git" -type d)
+repo_dirs=$(find -L $SEARCHDIR -name "*.git" -type d)
 count=$(echo "$repo_dirs" | wc -l)
-printf "$count found\n"
-repos=$(echo $repo_dirs | xargs dirname)
+printf "${count//[[:space:]]/} found\n"
+repos=$(echo $repo_dirs | xargs -n 1 dirname)
 
 get_remote_diff_log() {
   git log --branches --not --remotes --oneline --decorate --graph --color=always
 }
 
+changed_repos=0
 for repo in $repos; do
   ( cd $repo
     log_text=$(get_remote_diff_log)
@@ -35,6 +61,7 @@ for repo in $repos; do
     fi
     # Output, if needed
     if [[ -n "$no_up" || -n "$workchange" ]]; then
+      changed_repos=$((changed_repos+1))
       echo "$BOLD$RED$repo$NC:"
     fi
     if [[ -n "$no_up" ]]; then
@@ -46,3 +73,6 @@ for repo in $repos; do
   )
 done
 
+if [[ $changed_repos -eq 0 ]]; then
+  echo "No local-only commits found."
+fi
