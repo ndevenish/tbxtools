c6784cf63ae56698fda2fb3b0302211f9876f17b
Nicholas Devenish <ndevenish@gmail.com>
2018-12-19 12:11:26 +0000

    Use pathlib

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     package_dir={"": "src"},
     install_requires=[
         'enum34;python_version<"3"',
+        'pathlib;python_version<"3"',
         "docopt",
         "networkx~=2.1",
         "pyyaml",
